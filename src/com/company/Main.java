8c2ccc267432b8a32f6b0ed4937756b989a2c572
Vu Long <vubaolong2001@gmail.com>
2022-11-06 16:30:05 +0700

    my frist push and commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(" tong a va b = " +(a+b));
+    }
+}
